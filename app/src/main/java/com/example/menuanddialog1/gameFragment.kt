9bd61e62e034bd60f3aec148a6257dea19ec9b02
rifqi <rifqimaulanafernanda.rmf@gmail.com>
2020-03-18 21:42:15 +0700

    COMMIT

---
+++ app/src/main/java/com/example/menuanddialog1/gameFragment.kt
@@ -0,0 +1,47 @@
+package com.example.menuanddialog1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.menuanddialog1.databinding.FragmentGameBinding
+import com.example.menuanddialog1.databinding.FragmentGameOverBinding
+
+class gameFragment: Fragment() {
+    override fun onCreateView(
+        inflater:
+        LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentGameBinding>(
+            inflater,
+            R.layout.fragment_game,
+            container,
+            false
+        )
+        binding.submitButton.setOnClickListener { view: View ->
+            val checkedId =
+                binding.questionRadioGroup.checkedRadioButtonId
+            if (-1 != checkedId) {
+                var answerIndex = 0
+                when (checkedId) {
+                    R.id.firstAnswerRadioButton -> answerIndex = 0
+                    R.id.secondAnswerRadioButton -> answerIndex = 1
+                    R.id.thirdAnswerRadioButton -> answerIndex = 2
+                    R.id.fourthAnswerRadioButton -> answerIndex = 3
+                }
+                if (answerIndex == 0) {
+                    view.findNavController()
+                        .navigate(R.id.action_gameFragment_to_gameWonFragment)
+                } else {
+                    view.findNavController()
+                        .navigate(R.id.action_gameFragment_to_gameOverFragment)
+                }
+            }
+        }
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/menuanddialog1/titleFragment.kt
@@ -0,0 +1,31 @@
+package com.example.menuanddialog1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.menuanddialog1.databinding.FragmentTitleBinding
+
+
+class TittleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+        return binding.root
+    }
+}
